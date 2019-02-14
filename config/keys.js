0ec1026c8964c38354b1f2bba4a0273eeaa3dc22
PaulKlein22 <paulklein22@gmail.com>
2019-02-14 17:14:40 -0600

    adding environmental variables

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://paul:paul123@ds133865.mlab.com:33865/vegetabelly',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
