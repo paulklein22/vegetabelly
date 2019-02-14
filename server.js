4ff6d3fefad88c3d2e65c000ce7d4575165aae50
PaulKlein22 <paulklein22@gmail.com>
2019-02-14 17:44:02 -0600

    preparing for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const path = require('path');
 
 const search = require('./routes/api/search');
 const users = require('./routes/api/users');
@@ -34,6 +35,16 @@ app.use('/api/search', search);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// Serve Static Assets if in Production
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 2187;
 
