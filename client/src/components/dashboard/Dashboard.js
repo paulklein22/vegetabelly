1bb63aa7396824a1720360f68cced719d303ecbb
PaulKlein22 <paulklein22@gmail.com>
2019-02-20 18:45:46 -0600

    adding dashboard component

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profileActions';
+
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getCurrentProfile();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);
