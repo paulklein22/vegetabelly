ac120d6d9593f3d3711e5ec1291cfe6b22823b07
PaulKlein22 <paulklein22@gmail.com>
2019-02-26 18:50:16 -0600

    build file

---
+++ client/build/precache-manifest.d965368470f2a88b4a7d96d0f6224a46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64a88205530c344aa5268dd26579c9bb",
+    "url": "/static/media/spinner.64a88205.gif"
+  },
+  {
+    "revision": "a53d712520bc05e81115fa6f670708d0",
+    "url": "/static/media/logo.a53d7125.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "13099ace9b2686f9b274",
+    "url": "/static/js/main.13099ace.chunk.js"
+  },
+  {
+    "revision": "d48b5685c3a26db72860",
+    "url": "/static/js/2.d48b5685.chunk.js"
+  },
+  {
+    "revision": "13099ace9b2686f9b274",
+    "url": "/static/css/main.686fc1d9.chunk.css"
+  },
+  {
+    "revision": "66de47e6c2750bddeb646b070212326d",
+    "url": "/index.html"
+  }
+];
